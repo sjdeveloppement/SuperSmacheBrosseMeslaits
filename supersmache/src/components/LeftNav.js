13a88cb3ab51adc4d14ff554606b46dc0b241578
Serra jahan <jahan.serra@gmail.com>
2021-11-23 18:15:12 +0100

    add account menu

--- supersmache/src/components/LeftNav.js
+++ supersmache/src/components/LeftNav.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import home from '../img/home.svg';
+import rocket from '../img/rocket.svg';
+import user from '../img/user.svg';
 const LeftNav = () => {
     return (
         <div className="left-nav-container">
             <div className="icons">
                 <div className="icons-bis">
                     <NavLink to='/'  activeClassName="active-left-nav">
-                        <img src="./img/icons/home.svg" alt="home"/>
+                       <img  src={home}   alt="home"/>
                     </NavLink>
                     <br />
                     <NavLink to='/trending'  activeClassName="active-left-nav">
-                        <img src="./icons/rocket.svg" alt="trending"/>
+                        <img src={rocket}   alt="trending"/>
                     </NavLink>
                     <br />
                     <NavLink to='/profil'  activeClassName="active-left-nav">
-                        <img src="./icons/user.svg" alt="profil"/>
+                        <img src={user}   alt="profil"/>
                     </NavLink>
                 </div>
             </div>
--- supersmache/src/components/Profil/UpdateProfil.js
+++ supersmache/src/components/Profil/UpdateProfil.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LeftNav from '../LeftNav';
-
+import { useSelector } from "react-redux";
 const UpdateProfil = () => {
+    const userData = useSelector((state) => state.userReducer)
     return (
         <div className="profil-container">
             <h3>Modifier mon profil</h3>
             <LeftNav />
+            <h4> Profil de {userData.pseudo}</h4>
         </div>
     );
 };
